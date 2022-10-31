6b8b76d9050568a5464590ff711edbfac5f093ba
nakulsharma09 <nakul.57-cse-19@mietjammu.in>
2022-10-31 21:07:49 +0530

    third lab code added

---
+++ lab3/lab3.sh
@@ -0,0 +1,17 @@
+echo "Today's Date is - `date` "
+echo "The current username is - `whoami` "
+echo "The Present Directory is - `pwd` "
+
+#Mods
+
+#1.
+echo " New Directory is created `mkdir Lab3Dir` "
+
+#2.
+echo "Filename `ls -l` "
+
+#3.
+echo `cd Lab3Dir && touch lab3new.sh` 
+
+#4.
+echo `cp lab3.sh Lab3Dir/lab3new.sh`
