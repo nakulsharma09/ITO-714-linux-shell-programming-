90f6bc4b0ed43b45db6bd11712459f9543e19a59
nakulsharma09 <nakul.57-cse-19@mietjammu.in>
2022-10-31 20:43:51 +0530

    first commit

---
+++ lab2/lab2.sh
@@ -0,0 +1,8 @@
+echo "Enter the value of a"
+read a
+echo "Enter the value of b"
+read b
+echo "The value of a+b is $(($a+$b))"
+echo "The value of a-b is $(($a-$b))"
+echo "The value of a*b is $(($a*$b))"
+echo "The value of a/b is $(($a/$b))"
